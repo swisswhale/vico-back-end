af1bee6d380122e534dd971f9bab18e99c03bb84
swisswhale <jordan.p.mclendon@gmail.com>
2025-04-10 21:03:41 -0400

    tested to ensure backend was running with test route.

--- server.js
+++ server.js
@@ -8,4 +8,14 @@ import connectDB from './config/db.js';
 connectDB();
 
 const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('✅ Backend is running!');
+  });
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on http://localhost:${PORT}`);
+});
 // rest of your server...
