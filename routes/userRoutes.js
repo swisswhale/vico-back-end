f565f6b96b7588abb2cde89fa1e9223164ee4844
CloudArcTrpr <cjbells3002@gmail.com>
2025-04-14 18:44:41 -0400

    fix auth commit

---
+++ routes/userRoutes.js
@@ -0,0 +1,36 @@
+import express from 'express'
+const router = express.Router();
+
+import User  from '../models/User.js'
+
+import verifyToken from '../middleware/authMiddleware.js'
+
+router.get('/', verifyToken, async (req, res) => {
+  try {
+    const users = await User.find({}, "username");
+
+    res.json(users);
+  } catch (err) {
+    res.status(500).json({ err: err.message });
+  }
+});
+
+router.get('/:userName/:userId', verifyToken, async (req, res) => {
+  try {
+    if (req.user._id !== req.params.userId){
+      return res.status(403).json({ err: "Unauthorized"});
+    }
+
+    const user = await User.findById(req.params.userId);
+
+    if (!user) {
+      return res.status(404).json({ err: 'User not found.'});
+    }
+
+    res.json({ user });
+  } catch (err) {
+    res.status(500).json({ err: err.message });
+  }
+});
+
+export default router;
