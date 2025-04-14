eb1c1cbc2f672d0fb5e45e036f73a7136e504c13
CloudArcTrpr <cjbells3002@gmail.com>
2025-04-14 19:33:33 -0400

    add email check for /sign-up in authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -9,14 +9,15 @@ const saltRounds = 12;
 
 router.post('/sign-up', async (req, res) => {
   try {
-    const userInDatabase = await User.findOne({ username: req.body.username });
+    const userInDatabase = await (User.findOne({ username: req.body.username }) || User.findOne({ email: req.body.email }));
     
     if (userInDatabase) {
-      return res.status(409).json({err: 'Username already taken.'});
+      return res.status(409).json({err: 'Username already taken, OR Email already in use.'});
     }
     
     const user = await User.create({
       username: req.body.username,
+      email: req.body.email,
       hashedPassword: bcrypt.hashSync(req.body.password, saltRounds)
     });
 
