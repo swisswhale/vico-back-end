927c1881f21cfdf64c8733e0ac69abd3a1581a83
swisswhale <jordan.p.mclendon@gmail.com>
2025-04-16 18:20:28 -0400

    updated mongodb email index.

--- models/User.js
+++ models/User.js
@@ -6,7 +6,7 @@ import bcrypt from "bcryptjs";
 
 const userSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true, trim: true },
-    // email: { type: String, required: true, unique: true, lowercase: true },
+    // email: { type: String, required: true, unique: true, lowercase: true }, removed from mongodb
     password: {
         type: String,
         required: true,
