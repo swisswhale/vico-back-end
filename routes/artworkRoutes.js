292f9a34907f62b4e45d21f468d920ab9bf427f8
swisswhale <jordan.p.mclendon@gmail.com>
2025-04-17 20:55:48 -0400

    Updated artwork routes to with controller logic.

--- routes/artworkRoutes.js
+++ routes/artworkRoutes.js
@@ -1,4 +1,18 @@
 import express from 'express';
+import { searchArtworks. getArtwork, saveArtwork } from '../controllers/artworkController.js';
+import { authMiddleware } from '../middleware/authMiddleware.js';
+
+const router = express.Router();
+
+router.get('/search', authMiddleware, searchArtworks);
+router.get('/:id', authMiddleware, getArtwork);
+router.post('/save', authMiddleware, saveArtwork);
+
+export default router
+/*
+Old code for ref...
+
+import express from 'express';
 import {
     searchExternalArtworks,
     searchHarvardArtworksController,
@@ -139,3 +153,4 @@ router.get('/harvarts', async (req,res) => {
 
 
 export default router;
+*/
