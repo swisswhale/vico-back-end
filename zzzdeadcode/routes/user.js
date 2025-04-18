cfa5ff1129ab59400229777587593ded263b9ce5
swisswhale <jordan.p.mclendon@gmail.com>
2025-04-18 14:44:14 -0400

    Updated user route logic; moved deprecated code to deadcode directory

--- zzzdeadcode/routes/user.js
+++ zzzdeadcode/routes/user.js
@@ -1,14 +1,3 @@
-import express from 'express';
-import { getProfile, updateProfile } from '../controllers/userController.js';
-import { authMiddleware } from '../middleware/authMiddleware.js';
-
-const router = express.Router();
-
-router.get('/profile', authMiddleware, getProfile);
-router.put('/profile', authMiddleware, updateProfile);
-
-export default router;
-
 /*
 import express from 'express';
 import mongoose from 'mongoose';
