ddfaf74e5f58e58e0a6db9619571f78d004f30eb
swisswhale <jordan.p.mclendon@gmail.com>
2025-04-17 20:34:59 -0400

    Updated artwork controller to remove artsy.

--- controllers/artworkController.js
+++ controllers/artworkController.js
@@ -1,3 +1,44 @@
+import Artwork from '../models/Artwork.js';
+import { searchHarvardArtworks, getHarvardArtworkById } from '../services/harvardService.js';
+
+export const searchArtworks = async (req, res) => {
+    try {
+        const { query } = req.query;
+        const artworks = await searchHarvardArtworks(query);
+        res.json(artworks);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+export const getArtwork = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const artwork = await getHarvardArtworkById(id);
+        if(!artwork) {
+            return res.status(404).json({ message: 'Artwork not found' });
+        }
+        res.json(artwork);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+export const saveArtwork = async (req, res) => {
+    try {
+      const { harvardId } = req.body;
+      let artwork = await Artwork.findOne({ harvardId });
+      if (!artwork) {
+        const harvardArtwork = await getHarvardArtworkById(harvardId);
+        artwork = new Artwork(harvardArtwork);
+        await artwork.save();
+      }
+      res.status(201).json(artwork);
+    } catch (error) {
+      res.status(400).json({ message: error.message });
+    }
+  };
+/*
 // HarvardAPI Import
 import { searchHarvardArtworks, getHarvardArtworkById } from '../services/harvardService.js';
 // Artsy Import
@@ -43,3 +84,4 @@ export const getHarvardArtworkController = async (req, res) => {
         res.status(500).json({ error: 'Failed to fetch Harvard artwork', details: err.message });
     }
 };
+*/
